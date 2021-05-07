3d6d2e0cf94db53148b4e70ec7b12ab2b722244a
seunghyukcho <shhj1998@postech.ac.kr>
2021-05-07 20:39:28 +0900

    add pad provider

--- dataset.py
+++ dataset.py
@@ -1,6 +1,7 @@
+import torch
 import pandas as pd
 from torch.utils import data
-
+from torch.nn.utils.rnn import pad_sequence
 
 class Dataset(data.Dataset):
     def __init__(self, file_name, tokenizer, label=False):
@@ -20,4 +21,26 @@ class Dataset(data.Dataset):
             return x, self.data['Category'].iloc[idx]
         else:
             return x
+
+
+class PadBatch:
+    def __init__(self, inference=False):
+        super(PadBatch, self).__init__()
+        self.inference = inference
+
+    def __call__(self, batch):
+        sentences, lens = [], []
+        if self.inference:
+            for item in batch:
+                sentences.append(torch.LongTensor(item))
+                lens.append(len(item))
+            return pad_sequence(sentences, batch_first=True), torch.LongTensor(lens)
+        else:
+            labels = []
+            for item in batch:
+                x, y = item
+                sentences.append(torch.LongTensor(x))
+                lens.append(len(x))
+                labels.append(y)
+            return pad_sequence(sentences, batch_first=True), torch.LongTensor(labels), torch.LongTensor(lens)
 
