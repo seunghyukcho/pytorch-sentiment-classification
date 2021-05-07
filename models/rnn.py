da61f6c7c0e0e3cae620f0b92d30286fad127d1c
seunghyukcho <shhj1998@postech.ac.kr>
2021-05-07 20:40:46 +0900

    fixed forward for padded sequences

--- models/rnn.py
+++ models/rnn.py
@@ -7,22 +7,30 @@ def add_model_args(parser):
             help='Number of hidden units')
     group.add_argument('--n_layers', type=int, default=1,
             help='Number of layers')
+    group.add_argument('--embd_dim', type=int, default=1,
+            help='Embedding dimension')
 
 
 class Model(nn.Module):
-    def __init__(self, args, embd_dim):
+    def __init__(self, args, vocab_size):
         super().__init__()
 
         self.n_hids = args.n_hids
         self.n_layers = args.n_layers
         self.n_classes = args.n_classes
+        self.embd_dim = args.embd_dim
 
-        self.rnn = nn.RNN(embd_dim, self.n_hids, self.n_layers, batch_first=True)
+        self.embd = nn.Embedding(vocab_size, self.embd_dim, padding_idx=0)
+        self.rnn = nn.GRU(self.embd_dim, self.n_hids, self.n_layers, batch_first=True)
         self.decoder = nn.Linear(self.n_hids, self.n_classes)
 
-    def forward(self, x):
+    def forward(self, x, lens):
         self.rnn.flatten_parameters()
+        x = self.embd(x)
         x, _ = self.rnn(x)
+        lens = lens.unsqueeze(-1).repeat(1, self.n_hids).unsqueeze(1) - 1
+        x = x.gather(1, lens)
+        x = x.squeeze(1)
         x = self.decoder(x)
 
         return x
