3e94b82f407b42227b3422d07b36bc5c19018ba0
mth9406 <51608554+mth9406@users.noreply.github.com>
2021-05-19 21:45:45 +0900

    Update drnn.py
    todo: max pooling

--- models/drnn.py
+++ models/drnn.py
@@ -12,39 +12,60 @@ def add_model_args(parser):
 
 
 class Model(nn.Module):
-    def __init__(self, args, vocab_size):
+    def __init__(self, args, vocab_size, k):
         super().__init__()
 
         self.n_hids = args.n_hids
         self.n_layers = args.n_layers
         self.n_classes = args.n_classes
         self.embd_dim = args.embd_dim
-        
+        self.k = k # window size # todo: take it as an 'model argument'.
+
         self.embd = nn.Embedding(vocab_size, self.embd_dim, padding_idx=0)
         self.rnn = nn.GRU(self.embd_dim, self.n_hids, self.n_layers, batch_first=True, dropout= 0.1)
-        self.drop = nn.Dropdout(p=0.2)
+        self.drop = nn.Dropdout(p=0.05)
         self.mlp = nn.Sequential(
                 nn.BatchNorm1d(self.n_hids),
                 nn.Linear(self.n_hids, self.n_hids),
                 nn.ReLU()
             )
-        self.maxpool = nn.MaxPool1d()
-        self.mlp_final = nn.Sequential(
+        self.maxpool = nn.AdaptiveMaxPool1d(self.n_hids)
+
+        self.decoder = nn.Sequential(
             nn.Linear(self.n_hids, self.n_hids)
+            nn.ReLU(),
+            nn.Linear(self.n_hids, self.n_classes)
         )
-        self.decoder = nn.Linear(self.n_hids, self.n_classes)
 
     def forward(self, x, lens):
         # lens: list(a length of every sentence)
         self.rnn.flatten_parameters()
+        x = self.embd(x)
+        
+        # DGRU and MLP
+        n_tokens = x.shape[1] # including paddings.(# = k-1)
+        seq_len = n_tokens-self.k
+        h = torch.tensor([],device= x.device)
+        
+        for i in range(seq_len):
+            h_t, _ = self.rnn(x[:,i:i+self.k]) # output from last layer, the others.
+            h_t = self.drop(h_t)
+            h_t = self.mlp(h_t)
+            h= h.cat((h,h_t),dim= 1)
+            
+        # Max Pool
+        # todo.
+        h_reshape = torch.reshape(h,(-1,seq_len,self.n_hids))
+        h_reshape = torch.unsqueeze(h_reshape, -1) # to apply maxpool1d
+        pooled=torch.nn.functional.max_pool(
+            h_reshape,
+            kernal_size=[1,seq_len,1,1],
+            strides=[1,1,1,1],padding=0
+            )
+        pooled = torch.reshape(pooled, (-1,self.n_hids))
 
+        # decoder
+        # pooled: (batch, N, n_hids)
+        output = self.decoder(pooled)
 
-
-        # x = self.embd(x)
-        # x, _ = self.rnn(x)
-        # lens = lens.unsqueeze(-1).repeat(1, self.n_hids).unsqueeze(1) - 1
-        # x = x.gather(1, lens)
-        # x = x.squeeze(1)
-        # x = self.decoder(x)
-
-        return x
+        return output
