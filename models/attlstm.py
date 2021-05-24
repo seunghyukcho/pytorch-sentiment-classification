c0d573ef7effd26bddc1adc8dd265962e152a56f
mth9406 <51608554+mth9406@users.noreply.github.com>
2021-05-24 20:01:41 +0900

    Update attlstm.py

--- models/attlstm.py
+++ models/attlstm.py
@@ -3,13 +3,13 @@ from torch import nn
 
 def add_model_args(parser):
     group = parser.add_argument_group('model')
-    group.add_argument('--n_hids', type=int, default=128,
+    group.add_argument('--n_hids', type=int, default=300,
             help='Number of hidden units')
     group.add_argument('--n_layers', type=int, default=1,
             help='Number of layers')
     group.add_argument('--embd_dim', type=int, default=1,
             help='Embedding dimension')
-    group.add_argument('--k', type=int, default= 100,
+    group.add_argument('--k', type=int, default= 8,
             help='the number of aspects defined by the user')
 
 
@@ -29,7 +29,7 @@ class Model(nn.Module):
         self.sentence_embd = nn.Embedding(sentence_size, self.embd_dim, padding_idx=0)
         
         # rnn structure
-        self.rnn = nn.LSTM(self.embd_dim, self.n_hids, self.n_layers, batch_first=True, dropout= 0.3)
+        self.rnn = nn.LSTM(self.embd_dim, self.n_hids, self.n_layers, batch_first=True, dropout= 0.5)
         
         # AE structure
         self.Wt = nn.Linear(self.embd_dim, self.k)
@@ -42,7 +42,7 @@ class Model(nn.Module):
         self.decoder = nn.Linear(self.n_hids, self.n_classes)
 
 
-    def forward(self, w, s, lw, ls):
+    def forward(self, w, s, lw, ls, is_train= True):
         self.rnn.flatten_parameters()
         # embedding layer
         w, s = self.word_embd(w), self.sentence_embd(s) # (batch, n, self.embd_dim), (batch, m, self.embd_dim)
@@ -51,16 +51,21 @@ class Model(nn.Module):
         h,_ = self.rnn(w) # h: (batch, n, self.n_hids)
         # 2) sentence --> Auto encoder
         w, s = torch.mean(w, dim=1), torch.mean(s, dim=1) # w, s: (batch, self.embd_dim)
-        c_s = (w+s)/2.0 # AVERAGE, captures both target information and context information.
+        c_s = (w+s)/2.0 # AVERAGE, captures both target information and context information. (batch, self.embd_dim)
         q_t = self.softmax(self.Wt(cs)) # (batch, self.k)
-        t_s = (self.T.T@q_t.unsqueeze(-1)).squeeze() # (batch, self.embd_dim)
-        
-        # todo : d correction. 
-        d = self.tanh((h@self.Wa@t_s))  # (batch,n,self.embd_dim) (self.embd_dim, self.embd_dim) (batch, self.embd_dim) != (batch, n, 1)
+        t_s = q_t@self.T # (batch, self.k) (self.k, self.embd_dim) = (batch, self.embd_dim)
+        # t_s = (self.T.T@q_t.unsqueeze(-1)).squeeze() # (embd_dim, k) (batch, k, 1) = (batch, self.embd_dim)
+
+        # (batch,n,self.embd_dim) (self.embd_dim, self.embd_dim) = (batch, n, self.embd_dim)
+        d = self.tanh((h@(self.Wa)@(t_s.unsqueeze(-1))))  # (batch, n, self.embd_dim) (batch, self.embd_dim,1) != (batch, n, 1)
         p = self.softmax(d) # (batch, n, 1)
 
         # obtain sentence representation
         z_s = torch.sum(p*h, dim=1) # (batch, self.embd_dim)
-        output = self.decoder(z_s)
+        prob = self.decoder(z_s) # (batch, n_classes)
+        prob = self.softmax(prob)
 
-        return x
+        if is_train:
+            return prob, self.T, t_s, c_s
+        else:
+            return prob
