f2b8e8b20a3b3adf1f7b2fc67fa2d2ee94db25ed
mth9406 <51608554+mth9406@users.noreply.github.com>
2021-05-24 15:49:23 +0900

    Update and rename wordk.py to word_sentence.py

---
+++ tokenizers/word_sentence.py
@@ -0,0 +1,32 @@
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import TreebankWordTokenizer
+
+def add_tokenizer_args(parser):
+    group = parser.add_argument_group('tokenizer')
+    group.add_argument('--dict', type=str, 
+            help='Path to word dictionary file')
+    group.add_argument('--sentence_dict',type=str,
+            help='Path to sentence dictionary file')
+
+
+class Tokenizer():
+    def __init__(self, args):
+        with open(args.dict, 'r') as f:
+            words = f.read().splitlines()
+            self.word_dict = {word: idx for idx, word in enumerate(words)}
+        
+        with open(args.sentence_dict, 'r') as f:
+            sentences = f.read().splitlines()
+            self.sentence_dict = {s: idx for idx, s in enumerate(sentences)}            
+            
+        self.word_tokenizer = TreebankWordTokenizer()
+        
+    def get_vocab_size(self):
+        return len(self.dict) + 1
+
+    def tokenize(self, sentence):
+       
+        sentence_tokens = list(map(lambda x:self.sentence_dict[x] if x in self.sentence_dict else len(self.sentence_dict) , sent_tokenize(sentence)))
+        word_tokens = list(map(lambda x:self.word_dict[x] if x in self.word_dict else len(self.word_dict), self.word_tokenizer(sentence)))
+        
+        return word_tokens, sentence_tokens
