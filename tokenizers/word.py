76b2226698a666d79844f963779afb471b36d1f3
mth9406 <51608554+mth9406@users.noreply.github.com>
2021-05-25 19:14:22 +0900

    Update word.py

--- tokenizers/word.py
+++ tokenizers/word.py
@@ -2,6 +2,8 @@ def add_tokenizer_args(parser):
     group = parser.add_argument_group('tokenizer')
     group.add_argument('--dict', type=str, 
             help='Path to dictionary file')
+    group.add_argument('--k', type=int, 
+            help='window size')
 
 
 class Tokenizer():
@@ -9,12 +11,14 @@ class Tokenizer():
         with open(args.dict, 'r') as f:
             words = f.read().splitlines()
             self.dict = {word: idx for idx, word in enumerate(words)}
-
+            
+            self.k = args.k
+            
     def get_vocab_size(self):
         return len(self.dict) + 1
 
     def tokenize(self, sentence,k=1):
-        tokens = []
+        tokens = [0]*(k-1)
         for word in sentence.split():
             tokens.append(self.dict.get(word, len(self.dict)))
         return tokens
